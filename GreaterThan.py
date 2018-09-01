de9bd97afe41510e3f33f4de33adf580785ed11e
Nick7421 <42498037+Nick7421@users.noreply.github.com>
2018-09-01 14:26:07 -0500

    Add files via upload

---
+++ GreaterThan.py
@@ -0,0 +1,13 @@
+#This is my First Attempt at Code!!!
+import time
+
+
+y = 15
+x = int(input("Enter a number: "))
+print ('Is your number greater than Y?')
+time.sleep(3)
+if x > y:
+    print('X is Greater')
+else:
+    print('Y is Greater Try Again!')
+
