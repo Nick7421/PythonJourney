7bceaecf4b1041fda5c2ddbdce397beed73321f5
Nick7421 <42498037+Nick7421@users.noreply.github.com>
2018-09-21 09:07:24 -0500

    Add files via upload

---
+++ studentseconds.py
@@ -0,0 +1,13 @@
+#Nick Neiert ng7034mm
+
+#This program will calculate the number of seconds a user has studied for a test.
+
+print('This program will calcute your studied hours into seconds. Please enter \n your hours studied below.')
+
+hours_studied = float(input('Please enter how many hours you have studied for the test:'))
+seconds_studied = hours_studied * 60 * 60
+minutes_studied = hours_studied * 60
+
+
+print('The number of minutes you studied for the test is:', minutes_studied)
+print('\n The number of seconds you studied for the test is:', seconds_studied)
---
+++ triangle.py
@@ -0,0 +1,11 @@
+#Nick Neiert ng7034mm
+#Assignment 2
+print('This program will calculate the area of a triangle. Please enter the base and height below. \n')
+base = float (input('Please enter the base of the triangle: ') )
+height = float(input('Please enter the height of the triangle: '))
+
+area = 1/2 * (base * height)
+
+print ('\n The base of the traingle is:',base)
+print('\n The height of the triangle is: ', height)
+print('\n The area of the triangle is:', area)
